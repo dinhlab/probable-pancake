d042d3bdc46f57a495ad8b713782b57a88728e32
Dinh <119478119+dinh292@users.noreply.github.com>
2023-03-01 21:35:17 +0700

    update format

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css'
 import { useState, useEffect } from 'react'
 
 const api = {
-  key: process.env.REACT_APP_API_KEY,  
-  base: 'https://api.openweathermap.org/data/2.5/',
+  key: process.env.REACT_APP_API_KEY,
+  base: 'https://api.openweathermap.org/data/2.5/'
 }
 
-function App() {
+function App () {
   const [searchInput, setSearchInput] = useState('')
   const [searchCity, setSearchCity] = useState('')
   const [weatherInfo, setWeatherInfo] = useState('')
@@ -22,7 +21,9 @@ function App() {
         const response = await fetch(url)
         const data = await response.json()
         if (response.ok) {
-          setWeatherInfo(`${data.name}, ${data.sys.country}, ${data.main.temp}°C, ${data.weather[0].description}`)
+          setWeatherInfo(
+            `${data.name}, ${data.sys.country}, ${data.main.temp}°C, ${data.weather[0].description}`
+          )
           setErrorMessage('')
         } else {
           setErrorMessage(data.message)
@@ -51,17 +52,21 @@ function App() {
         />
         <button>Search</button>
       </form>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          {errorMessage ? (
-            <div style={{ color: 'red' }}>{errorMessage}</div>
-          ) : (
-            <div>{weatherInfo}</div>
+      {loading
+        ? (
+          <div>Loading...</div>
+          )
+        : (
+          <>
+            {errorMessage
+              ? (
+                <div style={{ color: 'red' }}>{errorMessage}</div>
+                )
+              : (
+                <div>{weatherInfo}</div>
+                )}
+          </>
           )}
-        </>
-      )}
     </>
   )
 }
