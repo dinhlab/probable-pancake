262163f6255e80070878293da7ab9df653bb7f74
Dinh <119478119+dinh292@users.noreply.github.com>
2023-06-04 23:27:32 +0700

    styled with Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { useState, useEffect } from 'react'
-
+import './App.css'
 const api = {
   key: process.env.REACT_APP_API_KEY,
   base: 'https://api.openweathermap.org/data/2.5/'
 }
-
 function App () {
   const [searchInput, setSearchInput] = useState('')
   const [searchCity, setSearchCity] = useState('')
   const [weatherInfo, setWeatherInfo] = useState('')
   const [loading, setLoading] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
-
   useEffect(() => {
     const fetchWeatherData = async () => {
       if (!searchCity) return
@@ -35,40 +33,45 @@ function App () {
     }
     fetchWeatherData()
   }, [searchCity])
-
   const handleSubmit = (e) => {
     e.preventDefault()
     setSearchCity(searchInput)
   }
-
   return (
-    <>
-      <form onSubmit={handleSubmit}>
+    <div className='min-h-screen bg-cover bg-center bg-fixed flex flex-col items-center justify-center bg-gray-100' style={{ backgroundImage: "url('https://picsum.photos/id/62/2000/1333/')" }}>
+      <form className='max-w-md mx-auto p-4 bg-white rounded shadow' onSubmit={handleSubmit}>
         <input
+          className='w-full px-4 py-2 mb-2 border border-gray-300 rounded'
           onChange={(e) => setSearchInput(e.target.value)}
           value={searchInput}
           type='text'
           placeholder='City'
         />
-        <button>Search</button>
+        <button
+          className='w-full px-4 py-2 bg-blue-500 text-white rounded'
+          type='submit'
+        >
+          Search
+        </button>
       </form>
       {loading
         ? (
-          <div>Loading...</div>
+          <div className='mt-4 text-center text-gray-500'>Loading...</div>
           )
         : (
           <>
             {errorMessage
               ? (
-                <div style={{ color: 'red' }}>{errorMessage}</div>
+                <div className='font-serif text-3xl text-white'>{errorMessage}</div>
                 )
               : (
-                <div>{weatherInfo}</div>
+                <div className='text-center mt-4'>
+                  <p className='font-serif text-3xl text-white'>{weatherInfo}</p>
+                </div>
                 )}
           </>
           )}
-    </>
+    </div>
   )
 }
-
 export default App
